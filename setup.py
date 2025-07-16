48aca30066318e786aa6ccf8717ca8afae196b89
VITresearchgroup2024 <161232967+VITresearchgroup2024@users.noreply.github.com>
2025-07-16 16:12:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,17 +8,19 @@ setup(
     version='0.1',
     description='SPHAK: Sequence-based Prediction of Host Analysis using k-mers',
     author='Vibin Ipe Thomas, Vinni N G, Ananya Prakash, Kavya S',
-    author_email='vibin@cmscollege.ac.in',  
+    author_email='vibin@cmscollege.ac.in',
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.7',
     include_package_data=True,
+    package_data={
+        'sphak': ['data/*.pkl'],  
+    },
     entry_points={
         'console_scripts': [
-        'sphak=sphak.cli:main',
+            'sphak=sphak.cli:main',
         ],
     },
-
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
